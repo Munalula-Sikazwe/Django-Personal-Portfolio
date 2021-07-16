9bf00c7469b0f3dcb4c2c9eecfca5a2aa3e99425
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-16 09:39:21 +0200

    added interests fields to models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class AboutMe(models.Model):
     name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100)
-    
+    interest1 = models.TextField(max_length=500)
+    interest2 = models.TextField(max_length=500)
