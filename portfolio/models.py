35ab306a2889b2375aacf842bfd7320be5416bcd
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-16 12:06:55 +0200

    cleaned up models.py file to pep8 specifications.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,30 +1,40 @@
 from django.db import models
 
+
 # Create your models here.
 class AboutMe(models.Model):
     class Meta:
         verbose_name_plural = "About Me Section"
+
     name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100)
     interest1 = models.TextField(max_length=500)
     interest2 = models.TextField(max_length=500)
-    email = models.EmailField(max_length=200,null=True)
+    email = models.EmailField(max_length=200, null=True)
+
     def __str__(self):
         return self.name
+
+
 class Service(models.Model):
     title = models.CharField(max_length=100)
     tools = models.CharField(max_length=200)
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     published_date = models.CharField(max_length=100)
     link = models.URLField(max_length=100)
     description = models.TextField(max_length=100)
+
+
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
     subject = models.CharField(max_length=100)
     message = models.TextField(max_length=500)
+
+
 class Education(models.Model):
     qualification = models.CharField(max_length=100)
     level = models.CharField(max_length=100)
