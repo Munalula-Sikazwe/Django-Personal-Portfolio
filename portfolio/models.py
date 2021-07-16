48cbcec3a62a5ea5eefab77f487c12457a55532a
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-16 11:13:12 +0200

    added a Services model

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,3 +8,9 @@ class AboutMe(models.Model):
     job_title = models.CharField(max_length=100)
     interest1 = models.TextField(max_length=500)
     interest2 = models.TextField(max_length=500)
+    def __str__(self):
+        return self.name
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    tools = models.CharField(max_length=200)
+
