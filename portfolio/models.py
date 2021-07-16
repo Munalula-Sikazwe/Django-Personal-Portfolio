084c30c82bb058506b3f4479c405148cf4e2cae7
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-16 12:11:13 +0200

    Changed About me plural to About me.

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class AboutMe(models.Model):
     class Meta:
-        verbose_name_plural = "About Me Section"
+        verbose_name_plural = "AboutMe"
 
     name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100)
