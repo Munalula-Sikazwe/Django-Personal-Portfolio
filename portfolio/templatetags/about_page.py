b85ef3e37dcca17b66a0ea067644f4bd41ac2783
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-22 19:37:01 +0200

    Refactored template tags to work in a specific directory.

--- portfolio/templatetags/about_page.py
+++ portfolio/templatetags/about_page.py
@@ -1,8 +1,9 @@
 from django.template import Library
 from portfolio.models import AboutMe
+
 register = Library()
 
-@register.inclusion_tag('Partials/_aboutme_section.html')
-def get_about_date():
+@register.inclusion_tag("Partials/_aboutme_section.html")
+def get_about_data():
     aboutme = AboutMe.objects.all()
     return {"aboutme":aboutme}
