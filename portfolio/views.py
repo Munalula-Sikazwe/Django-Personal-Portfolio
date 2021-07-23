54459550dedbb6b721657f22167232e9b802526d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-23 18:51:10 +0200

    created contact details controller.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import View
 from django.shortcuts import render
+from .models import Contact
 # Create your views here.
 
 class MainView(View):
     def get(self,request):
         return render(request,'home.html')
+    def post(self,request):
+        name = request.POST.get('name','Anonymous')
+        email = request.POST.get('email','Anonymous')
+        subject = request.POST.get('subject','Anonymous')
+        message = request.POST.get('message','Anonymous')
+        contact = Contact.objects.create(name=name,email=email,subject=subject,message=message)
+        contact.save()
+        
