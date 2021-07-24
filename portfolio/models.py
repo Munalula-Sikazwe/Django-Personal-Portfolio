f60b499437d3980fefd52f61df8a7e86242e7a94
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-24 22:18:04 +0200

    staged migration .

--- portfolio/models.py
+++ portfolio/models.py
@@ -83,6 +83,6 @@ class Work(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=100)
     level = models.CharField(max_length=3)
-  
+
     def __str__(self):
         return self.name
