c2e9d818407a8d097cd26e1a9a978899039a064c
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-07-24 22:09:05 +0200

    added an intro field to my skills section model.

--- portfolio/models.py
+++ portfolio/models.py
@@ -83,5 +83,6 @@ class Work(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=100)
     level = models.CharField(max_length=3)
+    introduction = models.TextField(max_length=500)
     def __str__(self):
         return self.name
